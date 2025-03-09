04415b238f75b2947ecf302dce0a161713ae9eef
AugKDavis <kalytiaa@oregonstate.edu>
2025-03-09 15:09:18 -0700

    renaming lep histogram script

---
+++ code/figures/2024/lepHistograms2024.R
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------------------------------------
+# Visualizing size-frequencies of Leptasterias in 2024
+# The purpose of this R script is to visualize belt transect data on Leptasterias 
+# at sites around San Juan Island. I will make an overall size-frequency histogram, as well as one
+# For each site!
+# ---------------------------------------------------------------------------------------------------------
+
+
+# ------------------
+#   Load libraries and dependencies!
+# ------------------
+#For reading files, manipulating, plotting, etc.
+library(tidyverse)
+
+#To get my data
+source("../../../../code/cleaning/2024Cleaning.R")
+
+getwd()
